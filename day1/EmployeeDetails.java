bc72136ac09493c6a759759a9e900da482b6827e
krkamalesh <krkamalesh@gmail.com>
2025-08-16 23:42:06 +0530

    commit

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		 		 String name = "John Doe";
+				 int age = 30;
+				 double salary = 55000.75;
+				 char gender = 'M';
+				 boolean acceptTerms = true;
+				 float rating = 4.5f;
+				 long mobileNumber = 9876543210L;
+				 int pinCode = 56001;
+				 byte luckyNumber = 7;
+System.out.println
+("Name : "+name+"\n"+
+"Age :" +age+"\n"+
+"Salary :" +salary+"\n"+
+"Gender :" +gender+"\n"+
+"Accept Terms :" +acceptTerms+"\n"+
+"Rating :" +rating+"\n"+
+"Mobile Number :" +mobileNumber+"\n"+
+"Pin Code :" +pinCode+"\n"+		
+"Lucky Number :" +luckyNumber);
+
+	}
+
+}
